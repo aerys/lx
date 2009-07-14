7e9e909262fcd0fd7f5035dd8de70e6b323bc09c
promethe <promethe@1fb1a5e3-7057-474a-b5f3-6ad1a8a54f2f>
2009-07-14 18:38:13 +0000

    Dedicated XSL stylesheet for google services (analytics)
    git-svn-id: svn://82.231.206.180/lx@38 1fb1a5e3-7057-474a-b5f3-6ad1a8a54f2f

--- trunk/src/misc/lx-config.php
+++ trunk/src/misc/lx-config.php
@@ -5,6 +5,9 @@ define('LX_NAMESPACE', 'http://lx.aerys.in');
 if (!defined('LX_ROOT'))
   define('LX_ROOT', realpath('..'));
 
+if (!defined('LX_DEBUG'))
+  define('LX_DEBUG', false);
+
 if (!defined('LX_TIMEZONE'))
   define('LX_TIMEZONE', 'Europe/Paris');
 
