68757c51f0916f8d9cb15c4a1c878bcb6091e2be
promethe <promethe@promethe-VirtualBox.(none)>
2011-03-19 17:29:34 +0100

    Fixed lxcli.php when using 'create' and creating useless symlinks

--- trunk/framework/lxcli.php
+++ trunk/framework/lxcli.php
@@ -1,5 +1,12 @@
 <?php
 
+define('LX_HOME',       substr($argv[0], 0, strrpos($argv[0], '/') + 1));
+define('N',             "\n");
+define('T',             "\t");
+define('SYS',           $argv[1]);
+define('CURRENT',       $argv[2]);
+define('DEBUG',         false);
+
 function copy_dir($src, $dst, $mkdir = false)
 {
   $dir = opendir($src);
@@ -58,12 +65,11 @@ function copy_ext($src, $ext, $dst)
   closedir($dir);
 }
 
-//-----------------------------------------------------------------------------------------------------
-
 function create($path, $name)
 {
   $out = T . 'directory: ' . $path;
-  if (!is_dir($path)) { die('HAN ERROR'); }
+  if (!is_dir($path))
+    die('\'' . $path . '\' is not a valid directory.');
 
   @mkdir($path . '/' . $name);
   @mkdir($path . '/' . $name . '/bin');
@@ -76,10 +82,10 @@ function create($path, $name)
 
   if (SYS === 'win')
   {
-    @mkdir($path . '/' . $name . '/src/lib/lx');
+    @mkdir($path . '/' . $name . '/lib/lx');
 
     //windows does not support any ln -s so we have to copy all the lib right into the project
-    copy_dir(LX_HOME, $path . '/' . $name . '/src/lib/lx');
+    copy_dir(LX_HOME, $path . '/' . $name . '/lib/lx');
     rm_rf($path . '/' . $name . '/lib/lx/project');
 
     copy_ext(LX_HOME . 'xsl/src', 'xsl', $path . '/' . $name . '/src/views');
@@ -87,7 +93,6 @@ function create($path, $name)
   else //any other unix based shell
   {
     exec('ln -s ' . LX_HOME . ' ' . $path . '/' . $name . '/lib/lx');
-    exec('ln -s ' . LX_HOME . '/xsl/src/* . xsl ' . $path . '/' . $name . '/src/views');
   }
 
   return $out;
@@ -112,8 +117,7 @@ function update($feature)
       break;
 
     case 'all':
-      $out  = config();
-      $out .= '\n';
+      $out = config();
       $out .= models();
       break;
 
@@ -128,7 +132,7 @@ function update($feature)
 function lib()
 {
   $out = 'This undocumented feature is windows only';
-  copy_dir(LX_HOME, CURRENT . '/src/lib/lx');
+  copy_dir(LX_HOME, CURRENT . '/lib/lx');
 }
 
 function config()
@@ -139,9 +143,11 @@ function config()
   {  }
 
   $out = exec('php --rc XSLTProcessor');
-  if (substr($out, 0, 1) == 'E') { die('You must enable xslt extension in your php . ini'); }
+  if (substr($out, 0, 1) == 'E')
+    die('You must enable xslt extension in your php.ini');
 
-  $out = exec('php src/lib/lx/php/src/scripts/lx-project . php ' . CURRENT . '/lx-project . xml > bin/lx-project . php');
+  $out = exec('php lib/lx/php/src/scripts/lx-project.php '
+              . CURRENT . '/lx-project.xml > bin/lx-project.php');
   return $out;
 }
 
@@ -159,49 +165,33 @@ function models()
 
   foreach($models as $model)
   {
-    $cmd = 'php -f src/lib/lx/php/src/scripts/lx-orm . php src/models/'
-           . $model . ' . xml lx-php-orm . xsl > bin/models/'
-           . $model . ' . php';
+    $cmd = 'php -f lib/lx/php/src/scripts/lx-orm.php src/models/'
+           . $model . '.xml lx-php-orm.xsl > bin/models/'
+           . $model . '.php';
 
     $out = exec($cmd);
   }
 
-  $out .= N . T . 'complete';
   return $out;
 }
 
 function help()
 {
-  $out = T . 'current options are :' . N;
-  $out .= T . '---------------------' . N . N;
-  $out .= T . T . '* create %project:' . N . T . T . T . 'to deploy a %project named project skeleton in your current directory' . N . N;
-  $out .= T . T . '* create %project in %dir :' . N . T . T . T . 'to deploy a %project named project in the specified %dir directory' . N . N;
-  $out .= T . T . '* update :' . N . T . T . T . 'update all project files (configuration and models)' . N . N;
-  $out .= T . T . '* update config :' . N . T . T . T . 'update configuration files only' . N . N;
-  $out .= T . T . '* update models :' . N . T . T . T . 'regenerate php models only' . N . N;
-  $out .= T . T . '* help :' . N . T . T . T . 'display help' . N . N;
-
-  return $out;
+  return 'Usage: /lxcli.sh [action]' . N
+    . 'Actions:' . N
+    . '  create %project' . T . T . 'Deploy a new project named %project in your current directory' . N
+    . '  create %project in %dir' . T . 'Deploy a new project named %project in the %dir directory' . N
+    . '  update' . T . T . T. 'Update all project files (configuration and models)' . N
+    . '  update config' . T . T . T . 'Update configuration files only' . N
+    . '  update models' . T . T . T . 'Update models only' . N
+    . '  help' . T . T . T . T . 'Display this message' . N;
 }
 
-//-----------------------------------------------------------------------------------------------------
+if (DEBUG)
+  print_r($argv);
 
-define('LX_HOME', substr($argv[0], 0, strrpos($argv[0], '/') + 1));
-define('N', "\n");
-define('T', "\t");
-define('SYS', $argv[1]);
-define('CURRENT', $argv[2]);
-define('DEBUG', false);
-
-//-----------------------------------------------------------------------------------------------------
-
-echo N . '=====' . N;
-echo 'LXCLI';
-echo N . '=====' . N . N;
-
-if (DEBUG) { print_r($argv); }
-
-if ($argc < 4) { die(help()); }
+if ($argc < 4)
+  die(help());
 
 switch($argv[3])
 {
@@ -213,14 +203,19 @@ switch($argv[3])
 
       if (isset($argv[5]) && $argv[5] == 'in')
       {
-        if (isset($argv[6])) { $path = $argv[6]; }
-        else { die('HAN ERROR'); }
+        if (isset($argv[6]))
+          $path = $argv[6];
+        else
+          die('error');
       }
-      else if (isset($argv[5])) { die('HAN ERROR'); }
+      else if (isset($argv[5]))
+        die('error');
 
       die(create($path, $name));
 
-    } else { die('HAN ERROR'); }
+    }
+    else
+      die('error');
     break;
 
   case 'create-in':
